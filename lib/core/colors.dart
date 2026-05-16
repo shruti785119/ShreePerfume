f5e58d31e54b34fdb04e4ec29bd49568122a4763
shruti785119 <shrutiparekh785@gmail.com>
2026-05-16 16:41:18 +0530

    Add files via upload

---
+++ lib/core/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF22C37E);
+  static const background = Color(0xFF0D0D0D);
+  static const card = Colors.white;
+  static const textLight = Colors.white70;
+}
